3230288f6d9823d2cbda56e0cdd5a87b12f0b28b
unknown <rk5857578@gmail.com>
2023-12-25 00:32:28 +0530

    update all code once again

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import { RouterProvider } from 'react-router-dom';
+import { AllRoutes } from './component/routes/routers.tsx';
+import { Provider } from 'react-redux';
+import { store } from './component/store/store';
+import { QueryClient, QueryClientProvider, } from 'react-query';
+
+const queryClient = new QueryClient()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <RouterProvider router={AllRoutes} />
+      </QueryClientProvider>
+
+    </Provider>
   </React.StrictMode>
 );
 
